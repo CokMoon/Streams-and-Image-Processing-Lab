cc9dcec047ffbc82c10361e2dee46d4b84d0ba16
CokMoon <27973045+CokMoon@users.noreply.github.com>
2022-07-15 23:08:00 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+/*
+ * Author: Ben Yang
+ * Student Number: A00955176
+ */
+
+const IOhandler = require("./IOhandler"),
+  zipFilePath = `${__dirname}/myfile.zip`,
+  pathUnzipped = `${__dirname}/unzipped`,
+  pathProcessed = `${__dirname}/grayscaled`;
