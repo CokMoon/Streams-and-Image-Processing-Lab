29f0a0fa954ba9211a99033800000c28dbbed70b
CokMoon <27973045+CokMoon@users.noreply.github.com>
2022-07-16 02:38:31 -0700

    completed

--- IOhandler.js
+++ IOhandler.js
@@ -1,11 +1,11 @@
 /*
- * File Name: IOhandler.js
- * Description: Collection of functions for files input/output related operations
- *
- * Created Date:
- * Author:
- *
+ * Author: Ben Yang
+ * Student Number: A00955176
+ * Modified Date: July 16, 2022
  */
+
+const { rejects } = require("assert");
+const { resolve } = require("path");
 
 const unzipper = require("unzipper"),
   fs = require("fs"),
@@ -19,7 +19,17 @@ const unzipper = require("unzipper"),
  * @param {string} pathOut
  * @return {promise}
  */
-const unzip = (pathIn, pathOut) => {};
+const unzip = (pathIn, pathOut) => {
+  const promise = fs
+    .createReadStream(pathIn)
+    .pipe(unzipper.Extract({ path: pathOut }))
+    .promise()
+    .then(
+      () => resolve("Extraction operation complete"),
+      (e) => rejects("error", e)
+    );
+  return promise;
+};
 
 /**
  * Description: read all the png files from given directory and return Promise containing array of each png file path
@@ -27,7 +37,20 @@ const unzip = (pathIn, pathOut) => {};
  * @param {string} path
  * @return {promise}
  */
-const readDir = (dir) => {};
+const readDir = (dir) => {
+  return new Promise((resolve, reject) => {
+    const pngFilePath = [];
+    fs.readdir(dir, (err, files) => {
+      if (err) reject(err);
+      files.forEach((file) => {
+        if (path.extname(path.join(dir, file)) === ".png") {
+          pngFilePath.push(path.join(dir, file));
+        }
+      });
+      resolve(pngFilePath);
+    });
+  });
+};
 
 /**
  * Description: Read in png file by given pathIn,
@@ -37,7 +60,33 @@ const readDir = (dir) => {};
  * @param {string} pathProcessed
  * @return {promise}
  */
-const grayScale = (pathIn, pathOut) => {};
+const grayScale = (pathIn, pathOut) => {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(pathIn)
+      .pipe(new PNG())
+      .on("parsed", function () {
+        for (var y = 0; y < this.height; y++) {
+          for (var x = 0; x < this.width; x++) {
+            var idx = (this.width * y + x) << 2;
+
+            // grayscale conversion
+            const gray =
+              0.2126 * this.data[idx] +
+              0.7152 * this.data[idx + 1] +
+              0.0722 * this.data[idx + 2];
+            this.data[idx] = gray;
+            this.data[idx + 1] = gray;
+            this.data[idx + 2] = gray;
+          }
+        }
+        this.pack().pipe(
+          fs.createWriteStream(path.join(pathOut, path.basename(pathIn)))
+        );
+        resolve();
+      })
+      .on("error", (err) => reject(err));
+  });
+};
 
 module.exports = {
   unzip,
--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 /*
  * Author: Ben Yang
  * Student Number: A00955176
+ * Modified Date: July 16, 2022
  */
 
 const IOhandler = require("./IOhandler"),
   zipFilePath = `${__dirname}/myfile.zip`,
   pathUnzipped = `${__dirname}/unzipped`,
   pathProcessed = `${__dirname}/grayscaled`;
+
+IOhandler.unzip(zipFilePath, pathUnzipped)
+  .then(() => IOhandler.readDir(pathUnzipped))
+  .then((filePaths) =>
+    filePaths.forEach((path) => {
+      IOhandler.grayScale(path, pathProcessed);
+    })
+  )
+  .catch((err) => console.log(err));
